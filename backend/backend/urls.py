3e3c50b0e369bf42d0ea3dd8960902e15ad9c677
vinaypatil-132 <vinaycp50@gmail.com>
2025-06-13 14:02:44 +0530

    updated scource code

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),  # <-- Add this line
+    path('trips/', include('api.urls')),
+    path('', lambda request: redirect('trips/')),  # Redirect root to /trips/
 ]
-
